0a58b76c7795d861d9b31a4856715b51dd089b76
daniNKV <nikiforovdaniel@gmail.com>
2024-04-21 23:46:25 -0300

    mostrar aviso de construcción en homepage

--- src/core/settings.py
+++ src/core/settings.py
@@ -132,6 +132,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR.parent / "local-cdn" / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
