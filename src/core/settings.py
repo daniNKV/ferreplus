a42441c4a79618ccf6decb78c49445a372166ec9
daniNKV <nikiforovdaniel@gmail.com>
2024-04-21 21:23:16 -0300

    agregar dependencias temporales del frontend

--- src/core/settings.py
+++ src/core/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "templates" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
