d5df38c425f89747a8763305e35810b920ee257a
LeChonCito <contiaugusto07@gmail.com>
2024-05-08 12:27:46 -0300

    Agregué formulario para cargar los items

---
+++ src/item/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class ItemForm(forms.Form):
+    name = forms.CharField(max_length=30)
+    description = forms.CharField(max_length=200)
+    creation_date = forms.DateField(required=False)
