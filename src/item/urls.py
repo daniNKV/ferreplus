d8a3d7562fffd9c63dfe9ed773c0efbe572c6625
Daniel Nikiforov <83901326+daniNKV@users.noreply.github.com>
2024-05-14 22:41:31 +0000

    quitar rutas incorrectas

--- src/item/urls.py
+++ src/item/urls.py
@@ -3,7 +3,6 @@ from . import views
 from item import views
 
 urlpatterns = [
-    path("", include("allauth.urls")),
     path("", views.itemList, name="itemList"),
     path("subirArticulo/", views.createItem, name="createItem"),
     path("<int:item_id>/delete", views.deleteItem, name="deleteItem"),
