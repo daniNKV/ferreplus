aee5c07d10ee4a074fd3e46a8407f6c657a20a14
daniNKV <nikiforovdaniel@gmail.com>
2024-05-15 11:51:16 -0300

    redimensionar avatar al subirlo

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -6,13 +6,15 @@ from .jobs import UploadToPathAndRename
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    avatar = models.ImageField(default="avatars/avatar-default.jpg", upload_to=UploadToPathAndRename('avatars'))
+    avatar = models.ImageField(
+        default="avatars/avatar-default.jpg", upload_to=UploadToPathAndRename("avatars")
+    )
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        path = self.avatar.path # pylint: disable=no-member
+        path = self.avatar.path  # pylint: disable=no-member
         img = Image.open(path)
-        if img.height > 700 or img.width > 700:
-            output_size = (700, 700)
-            img.thumbnail(output_size)
+        if img.height != 500 or img.width != 500:
+            output_size = (500, 500)
+            img = img.resize(output_size)
             img.save(path)
