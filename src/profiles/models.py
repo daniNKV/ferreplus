f7f5f8e0e86d6216d2fb85ebaaceb85b9df15759
daniNKV <nikiforovdaniel@gmail.com>
2024-05-15 11:51:16 -0300

    redimensionar avatar al subirlo y envio de mensajes

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -6,13 +6,19 @@ from .jobs import UploadToPathAndRename
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    avatar = models.ImageField(default="avatars/avatar-default.jpg", upload_to=UploadToPathAndRename('avatars'))
+    avatar = models.ImageField(
+        default="avatars/avatar-default.jpg", upload_to=UploadToPathAndRename("avatars")
+    )
+    avatar = models.ImageField(
+        default="avatars/avatar-default.jpg", upload_to=UploadToPathAndRename("avatars")
+    )
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        path = self.avatar.path # pylint: disable=no-member
+        path = self.avatar.path  # pylint: disable=no-member
+        path = self.avatar.path  # pylint: disable=no-member
         img = Image.open(path)
-        if img.height > 700 or img.width > 700:
+        if img.height != 700 or img.width != 700:
             output_size = (700, 700)
-            img.thumbnail(output_size)
+            img = img.resize(output_size)
             img.save(path)
--- src/static/js/base.js
+++ src/static/js/base.js
@@ -1,8 +1,6 @@
 window.onscroll = function() {
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 0) {
-        navbar.classList.add('fixed');
-        navbar.classList.add('inset-0');
         navbar.classList.add('h-16');
     }
 };
