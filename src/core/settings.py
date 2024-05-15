1b3542fb1a48b4c444314ffa6f3079f82276ec7c
LeChonCito <contiaugusto07@gmail.com>
2024-05-15 10:22:42 -0300

    Arreglar las imagénes de artículos

--- src/core/settings.py
+++ src/core/settings.py
@@ -141,6 +141,10 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = BASE_DIR.parent / "local-cdn" / "static"
 # Default primary key field type
