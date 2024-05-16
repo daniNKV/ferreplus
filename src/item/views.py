13e7eae7c539be0def1b12dda88e84521eb3f5a7
daniNKV <nikiforovdaniel@gmail.com>
2024-05-15 23:37:14 -0300

    permitir al usuario eliminar un articulo publicado

--- src/item/views.py
+++ src/item/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST, require_http_methods
 from item.models import Item
 from .forms import ItemForm
 
@@ -24,32 +26,35 @@ def create_item(request):
         form = ItemForm()
     return render(request, "item/create_item.html", {"form": form})
 
+
 @login_required
 def edit_item(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
     if item.user != request.user:
-        return redirect('profile_view', user_id=request.user.id)
-    if request.method == 'POST':
+        return redirect("profile_view", user_id=request.user.id)
+    if request.method == "POST":
         form = ItemForm(request.POST, request.FILES, instance=item)
         if form.is_valid():
-            if 'image' in request.FILES:
-                item.image = request.FILES['image']
+            if "image" in request.FILES:
+                item.image = request.FILES["image"]
             form.save()
             messages.success(request, "¡Artículo modificado exitosamente!")
-            return redirect('profile_view', user_id=request.user.id)
+            return redirect("profile_view", user_id=request.user.id)
     else:
         form = ItemForm(instance=item)
-    return render(request, 'item/edit_item.html', {'form': form})
+    return render(request, "item/edit_item.html", {"form": form})
 
+
+@login_required
+@require_POST
 def delete_item(request, item_id):
-    if request.method == "POST":
-        item = get_object_or_404(
-            Item, pk=item_id
-        )  # agregar desp como param user = request.user (solo acá)
-        item.delete()
-        messages.success(request, "¡Artículo eliminado exitosamente!")
-    return redirect("itemList")
-
+    item = get_object_or_404(Item, pk=item_id)
+    if item.user != request.user:
+        messages.success(request, "¡El articulo no es tuyo!")
+        return redirect("profile_view", user_id=request.user.id)
+    item.delete()
+    messages.success(request, "¡Artículo eliminado exitosamente!")
+    return redirect("profile_view", user_id=request.user.id)
 
 # def detail_item(request, item_id):
 #     if request.method == "POST":
