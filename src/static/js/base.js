5e0da974bf9bb71c12bddb10c62d5de305e172ea
daniNKV <nikiforovdaniel@gmail.com>
2024-05-24 01:22:01 -0300

    mejorar responsiveness de la landing page

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     window.onscroll = () => {
         let navbar = document.getElementById('navbar');
         if (window.scrollY > 0) {
-            console.log("scrolling")
             navbar.classList.add('h-16')
             navbar.classList.remove('h-20');
     
